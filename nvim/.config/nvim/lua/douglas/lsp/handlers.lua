c100c6841947b8e11dc8538b50e4a2e2b1a24d60
Douglas Rumbaugh <dbr4@psu.edu>
2022-10-31 14:23:58 -0400

    nvim: LSP interface adjustment
    Removed accesses to a few deprecated interfaces in the lsp configuration
    for neovim.

--- nvim/.config/nvim/lua/douglas/lsp/handlers.lua
+++ nvim/.config/nvim/lua/douglas/lsp/handlers.lua
@@ -49,7 +49,7 @@ end
 
 local function lsp_highlight_document(client)
   -- Set autocommands conditional on server_capabilities
-  if client.resolved_capabilities.document_highlight then
+  if client.server_capabilities.document_highlight then
     vim.api.nvim_exec(
       [[
       augroup lsp_document_highlight
@@ -83,7 +83,7 @@ end
 
 M.on_attach = function(client, bufnr)
   if client.name == "tsserver" then
-    client.resolved_capabilities.document_formatting = false
+    client.server_capabilities.document_formatting = false
   end
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
@@ -96,6 +96,6 @@ if not status_ok then
   return
 end
 
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+M.capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 
 return M
