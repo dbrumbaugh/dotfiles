19e7b0bb3dceb526229092c8b7ff9779dd298104
Douglas Rumbaugh <dbr4@psu.edu>
2022-11-03 13:09:16 -0400

    nvim: Removed special transparency settings
    I've switched to using Picom transparency rules, from the kitty setting,
    and so now I can have transparency on highlights, etc. in nvim. No need
    to disable them all.

--- nvim/.config/nvim/lua/douglas/colorscheme.lua
+++ nvim/.config/nvim/lua/douglas/colorscheme.lua
@@ -6,29 +6,5 @@ if not status then
     return
 end
 
-if colorscheme == "gruvbox" then
-    vim.cmd("hi clear CursorLine")
-    vim.cmd("hi CursorLine gui=underline cterm=underline")
-
-    vim.cmd("hi Normal guibg=NONE")
-    vim.cmd("hi SignColumn guibg=NONE")
-    vim.cmd("hi CursorLineNr guibg=NONE")
-
-    vim.cmd("hi GruvboxAquaSign guibg=NONE")
-    vim.cmd("hi GruvboxBlueSign guibg=NONE")
-    vim.cmd("hi GruvboxGreenSign guibg=NONE")
-    vim.cmd("hi GruvboxOrangeSign guibg=NONE")
-    vim.cmd("hi GruvboxPurpleSign guibg=NONE")
-    vim.cmd("hi GruvboxYellowSign guibg=NONE")
-    vim.cmd("hi GruvboxRedSign guibg=NONE")
-
-    vim.cmd("hi GitSignsAdd guibg=NONE")
-    vim.cmd("hi GitSignsChange guibg=NONE")
-    vim.cmd("hi GitSignsDelete guibg=NONE")
-    vim.cmd("hi DiagnosticSignError guibg=NONE")
-    vim.cmd("hi DiagnosticSignHint guibg=NONE")
-    vim.cmd("hi DiagnosticSignInfo guibg=NONE")
-    vim.cmd("hi DiagnosticSignWarn guibg=NONE")
-    vim.opt.pumblend = 15
-    vim.opt.winblend = 15
-end
+vim.opt.pumblend = 15
+vim.opt.winblend = 15
