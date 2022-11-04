caac101b0dd909333bb91aa62f7332b33cc03987
Douglas Rumbaugh <doug@douglasrumbaugh.com>
2022-11-03 23:01:14 -0400

    nvim: revised completion keybindings
    Tab was used to both navigate up and down the completion menu,
    and also to advance to the next part of a snippet to fill in. This
    effectively made snippets useless--as you'd need to quit out of the
    completion dialog before you could advance to the next thing.
    
    Now tab is strictly for advancing--the completion menu is navigated
    with C-n and C-p. This'll take a bit of muscle memory retraining.

--- nvim/.config/nvim/lua/douglas/completions.lua
+++ nvim/.config/nvim/lua/douglas/completions.lua
@@ -53,8 +53,9 @@ cmp.setup {
     end,
   },
   mapping = {
-    ["<C-k>"] = cmp.mapping.select_prev_item(),
-		["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+
     ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
@@ -67,9 +68,7 @@ cmp.setup {
     -- Set `select` to `false` to only confirm explicitly selected items.
     ["<CR>"] = cmp.mapping.confirm { select = false },
     ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expandable() then
+      if luasnip.expandable() then
         luasnip.expand()
       elseif luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
@@ -83,9 +82,7 @@ cmp.setup {
       "s",
     }),
     ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
+      if luasnip.jumpable(-1) then
         luasnip.jump(-1)
       else
         fallback()
