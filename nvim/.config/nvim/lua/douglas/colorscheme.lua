e9b9acfcc5b014f603463280f87dffc78afdc4df
Douglas Rumbaugh <doug@douglasrumbaugh.com>
2023-01-25 14:46:45 -0500

    nvim: added color column

--- nvim/.config/nvim/lua/douglas/colorscheme.lua
+++ nvim/.config/nvim/lua/douglas/colorscheme.lua
@@ -8,3 +8,16 @@ end
 
 vim.opt.pumblend = 15
 vim.opt.winblend = 15
+
+-- Set the color column to help enforce
+-- code width
+str = ""
+for i=120,256,1
+do
+    str = str .. i .. ","
+end
+str = str .. 256
+
+vim.opt.colorcolumn = str
+-- Can set the colorcolumn to red by uncommenting this
+--vim.api.nvim_set_hl(0, "ColorColumn", {bg="#fb4934"})
