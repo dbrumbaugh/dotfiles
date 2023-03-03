1bc2a5e5c59717405bd9705e4e452eb859be98e2
Douglas Rumbaugh <dbr4@psu.edu>
2023-03-03 16:12:51 -0500

    vis: enabled auto-indent

--- vis/.config/vis/visrc.lua
+++ vis/.config/vis/visrc.lua
@@ -4,12 +4,12 @@ require('vis')
 require('vis')
 
 vis.events.subscribe(vis.events.INIT, function()
-	-- Your global configuration options
+    -- Your global configuration options
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
-	-- Your per window configuration options e.g.
-	-- vis:command('set number')
+    -- Your per window configuration options e.g.
+    -- vis:command('set number')
     vis:command('set theme gruvbox')
     vis:command('set nu')
     vis:command('set rnu')
@@ -19,4 +19,5 @@ vis.events.subscribe(vis.events.WIN_OPEN, function(win)
     vis:command('set et')
     vis:command('set tw 4')
     vis:command('set ic')
+    vis:command('set ai')
 end)
